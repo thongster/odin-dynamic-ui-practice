8e5f73acf7ba103cc011ce37ebadf48dbef2835c
thongster <106802301+thongster@users.noreply.github.com>
2025-11-03 14:30:10 +0700

    added carousel logic by using transform translateX

--- script.js
+++ script.js
@@ -14,10 +14,37 @@ function hamburger() {
 function slideShow() {
     const previousButton = document.querySelector(".previous")
     const nextButton = document.querySelector(".next")
+    const images = document.querySelector(".images")
+    const imageList = document.querySelectorAll(".images img")
+    let imagePlace = imageList.length - imageList.length
+    let moveImage = 0
 
     nextButton.addEventListener("click", () => {
-        
+        if (imagePlace === imageList.length - 1) {
+            imagePlace = imageList.length - imageList.length
+            moveImage = 0
+            images.style.transform = `translateX(${moveImage}%)`
+            return;
+        }
+        moveImage = ((imagePlace + 1) * 100)
+        images.style.transform = `translateX(-${moveImage}%)`
+        imagePlace++
     })
+
+    previousButton.addEventListener("click", () => {
+        if (imagePlace === imageList.length - imageList.length) {
+            imagePlace = imageList.length - 1
+            moveImage = ((imagePlace) * 100)
+            images.style.transform = `translateX(-${moveImage}%)`
+            return;
+        }
+        moveImage = ((imagePlace - 1) * 100)
+        images.style.transform = `translateX(-${moveImage}%)`
+        imagePlace--
+    })
+
+
 }
 
 hamburger();
+slideShow()
